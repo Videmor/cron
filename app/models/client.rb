ed5945780631223675b25e4a8b0ef51dfa0234a1
videmor <videmor@gmail.com>
2013-07-20 08:45:15 -0500

    init

---
+++ app/models/client.rb
@@ -0,0 +1,20 @@
+class Client < ActiveRecord::Base
+  attr_accessible :age, :name
+
+
+  def self.new_client
+    create!(name: g_name, age: g_edad)
+    puts "new client"
+  end
+
+  private
+
+    def self.g_name
+      ('a'..'z').to_a.shuffle.join[0..5]
+    end
+
+    def self.g_edad
+      (0..9).to_a.shuffle.join[0..1]
+    end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cron::Application.config.secret_token = 'e49132edd9775d6115c7243d1f1879372182902b780ca6add6fd7819c1ae41132b647885c00240a8e1fb4962a50a03777fe71931ee8fa4dea3d76ff94457334f'
